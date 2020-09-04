b07b62fb9bc6a82f469143faaa1ed4d12aed8adf
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2020-09-04 16:52:23 +0900

    Issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,5 +1,4 @@
 class CalendarsController < ApplicationController
-
   # １週間のカレンダーと予定が表示されるページ
   def index
     getWeek
@@ -34,7 +33,11 @@ class CalendarsController < ApplicationController
       plan = plans.map do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+      wday_num = (@todays_date.wday + x)
+      if wday_num >= 7
+        wday_num = wday_num - 7
+      end
+      days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, wday: wdays[wday_num]}
       @week_days.push(days)
     end
 
