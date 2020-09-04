e0ebb2843071c6c6b08d176b9a5f4047b0d5804c
yoshikazu-ishitsuka <align.prog.flex.0812@gmail.com>
2020-09-04 18:02:47 +0900

    Revert "Merge pull request #9 from yoshikazu-ishitsuka/second_training"
    This reverts commit d4d4a59b31eac75ea6faf0fbdb88ed300e37647a, reversing
    changes made to 2704bd644a9389772907f25216d3b78d9d90500a.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,4 +1,5 @@
 class CalendarsController < ApplicationController
+
   # １週間のカレンダーと予定が表示されるページ
   def index
     get_week
@@ -14,7 +15,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:plan).permit(:date, :plan)
+    params.require(:calendars).permit(:date, :plan)
   end
 
   def get_week
@@ -33,12 +34,9 @@ class CalendarsController < ApplicationController
       plan = plans.map do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      wday_num = (@todays_date.wday + x)
-      if wday_num >= 7
-        wday_num = wday_num - 7
-      end
-      days = { month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans, wday: wdays[wday_num]}
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
       @week_days.push(days)
     end
+
   end
 end
